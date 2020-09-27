c067e6a11baeb80b2f4a1b512980816010e99e4a
Eng_Obada Qasrawi <Eng_Obada Qasrawi@LAPTOP-PBVM3AQ5>
2020-09-27 19:43:24 +0300

    inital2

--- Apartment_Search_App/src/test/java/Test.java
+++ Apartment_Search_App/src/test/java/Test.java
@@ -2,6 +2,7 @@
 
 import static org.junit.Assert.assertTrue;
 
+import java.awt.List;
 import java.util.ArrayList;
 
 import io.cucumber.java.en.Given;
@@ -9,10 +10,16 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Test {
-	
+	public   ArrayList <String> table  ;
 
 		@Given("these homes are contained in the system")
 		public void theseHomesAreContainedInTheSystem(io.cucumber.datatable.DataTable dataTable) {
+			
+			
+			
+			
+			
+			
 		    // Write code here that turns the phrase above into concrete actions
 		    // For automatic transformation, change DataTable to one of
 		    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
@@ -25,8 +32,8 @@ public class Test {
 
 
 
-		@When("I search about home by \"\"HOUSE")
-		public void iSearchAboutHomeByHOUSE() {
+		@When("I search about home by {string}")
+		public void iSearchAboutHomeBy(String string) {
 		    // Write code here that turns the phrase above into concrete actions
 		    throw new io.cucumber.java.PendingException();
 		}
@@ -37,5 +44,15 @@ public class Test {
 		}
 
 
+		@When("I search about home with price less than {int}")
+		public void iSearchAboutHomeWithPriceLessThan(Integer int1) {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new io.cucumber.java.PendingException();
+		}
+		@Then("A list of homes that matches the price specification should be returned and printed on the console")
+		public void aListOfHomesThatMatchesThePriceSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new io.cucumber.java.PendingException();
+		}
 
 }
