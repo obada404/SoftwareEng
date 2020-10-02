612b55180b2d1c48e418ebee6e5939edfb68ea72
Eng_Obada Qasrawi <Eng_Obada Qasrawi@LAPTOP-PBVM3AQ5>
2020-10-02 14:43:57 +0300

    5th

--- Apartment_Search_App/src/test/java/project2020/Test.java
+++ Apartment_Search_App/src/test/java/project2020/Test.java
@@ -29,18 +29,8 @@ public class Test {
 				System.arraycopy(ent.getKey().split("_"), 0	, arr, 0, 5);
 				System.arraycopy(ent.getValue().split("_"), 0, arr, 5, 5);
 			    w.setApartment(arr);
-			}
+			}   
 			
-			
-			
-		    // Write code here that turns the phrase above into concrete actions
-		    // For automatic transformation, change DataTable to one of
-		    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-		    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-		    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-		    //
-		    // For other transformations you can register a DataTableType.
-		   
 		}
 
 
